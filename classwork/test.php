da4b95f74e469666b9812b633c322274efd4b3c8
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-02-03 18:28:49 -0500

    variable definition

--- classwork/test.php
+++ classwork/test.php
@@ -3,13 +3,19 @@
         First Website
     </title>
 </head>
-
+<!-- The shortcut for commenting out a 
+     line of code in any language on windows is ctrl+/ -->
 <body>
-    Line One: 
-    
+    <br><br>Line One: <br><br>
+
     <?php
     echo "Hello World,";
-    ?>
 
+    $var1 = 2;
+    $var2 = 4;
+    $var3 = 6;
+    
+    ?>
+    
     how are you?
 </body>
