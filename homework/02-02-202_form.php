84dd12a2cf1c0f19bd433bbdd0d2de54315fd0d3
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-02-08 18:51:17 -0500

    form edits

--- homework/02-02-202_form.php
+++ homework/02-02-202_form.php
@@ -1,12 +1,20 @@
 <html>
 <head>
 <title>
-    Travel Page Form Below: <br><br>
+    Cruise Entry Form 
 </title>
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <div style="background-color:navy;color:white;padding:3%;">
+<style>
+  /* .fontSize{ 
+    font-weight: 50000;
+  }
+  #cruiseTitleStyle{
+    font-size: 100px;
+}*/
+</style>
+<h2 id="cruiseTitleStyle" class="fontSize"><u>Cruise Entry Form</u></h2>
   <h4>Type your first name below:</h4>
 <form>
   <input type="text">
@@ -17,10 +25,25 @@
   <input type="text">
 </form>
 
-<form name="form1" id="form1" action="/action_page.php">
-  Select your destination below: <br><br>
-  <select name="subject" id="subject">
+<form name="form1" id="form1" > <!-- action="/action_page.php" -->
+  Select your state of departure below: <br><br>
+  <select> <!-- name="subject" id="subject" -->
     <option value="" selected="selected">Select State</option>
+    <option value="" >CT </option>
+    <option value="" >NY </option>
+    <option value="" >NJ </option>
+    <option value="" >NH </option>
+    <option value="" >MA </option>
+    <option value="" >VT </option>
+    <option value="" >RI </option>
+  </select>
+  <br>
+</form>
+
+<form action="handler.php" method="post" name="form1" id="form1" > <!-- action="/action_page.php" -->
+  Select your destination below: <br><br>
+  <select> <!-- name="subject" id="subject" -->
+    <option value="" selected="selected">Select Destination</option>
     <option value="" >Eastern Caribbean </option>
     <option value="" >Southern Caribbean </option>
     <option value="" >Western Caribbean </option>
@@ -28,8 +51,9 @@
     <option value="" >Bahamas </option>
   </select>
   <br><br>
+
+<button type="submit" >Submit</button>
 </form>
-<button type="submit">Submit</button>
 </div>
 
 <div style="padding:3%;">
