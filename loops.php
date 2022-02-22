8f639c867e26d9fdf5bdbbe8ce353f3970458d4a
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-02-22 18:05:09 -0500

    loops

---
+++ loops.php
@@ -0,0 +1,22 @@
+<?php
+
+/*  - 4 kinds of Loops:
+    - While
+    - Do/While
+    - For
+    - Foreach
+*/
+
+for ($var=1; $var<=10; $var++){
+    echo "Your nnumber is: $var<br>";
+}
+
+while ($var < 10) {
+    //code
+}
+
+do{
+    //code
+} while ($var < 10);
+
+?>
