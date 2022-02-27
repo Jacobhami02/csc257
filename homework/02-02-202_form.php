ab86b0f46b389dfdac57dbdab26e7b6a94c4f473
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-02-26 19:05:11 -0500

    function statearray return

--- homework/02-02-202_form.php
+++ homework/02-02-202_form.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+
+include "functions.php";
+
 $stateArray = array(
     "Connecticut",
     "New York",
---
+++ homework/functions.php
@@ -0,0 +1,29 @@
+<?php
+
+// A simple funtion to echo out "hello" to the screen
+
+//Remember void funtions and value-returning functions from C++
+
+//Use include fileName.php to link other pages of code to the file in use.
+
+// function sayHello($name) {
+//     return "Hello $name!";
+// }
+
+// echo sayHello("Jim");
+
+
+function stateArrayReturn($stateName){
+    $stateArray = array(
+        "CT"=>"Connecticut",
+        "NY"=>"New York",
+        "NJ"=>"New Jersey",
+        "RI"=>"Rhode Island",
+        "MA"=>"Massachusetts"
+    );
+    return $stateArray[$stateName];
+}
+
+
+
+?>
