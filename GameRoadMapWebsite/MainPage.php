bb5f3a8f837ad5821c6b5a9f8f4a1457570e8f98
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-03-24 22:28:13 -0400

    visual foundation

---
+++ GameRoadMapWebsite/MainPage.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="styling.css">
+<title>Raindrop Roadmap</title>
+
+<body class='background'>
+<div id='rcorners'><h1 id='header'>Reign-drop Roadmap</h1></div>
+</body>
+</html>
