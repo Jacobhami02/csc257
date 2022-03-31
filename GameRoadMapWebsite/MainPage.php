b75c28f68b939e6e069cfa3fce04a400cc7d3c1b
Jacob Hamilton <jacobhami02@outlook.com>
2022-03-30 23:24:46 -0400

    corners

--- GameRoadMapWebsite/MainPage.php
+++ GameRoadMapWebsite/MainPage.php
@@ -5,12 +5,13 @@
 
 <body class="background">
 <!-- <div id='rcorners'><h1 id='header'>Reign-drop Roadmap</h1></div> -->
-<ul>
+<ul id="rcorners">
   <li><a class="logo"></a></li>
   <li><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Contact Us</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About Us</a></li>
 </ul>
+<div></div>
 
 </body>
 </html>
