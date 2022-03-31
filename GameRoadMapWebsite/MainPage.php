9cdf514da9df09a731b830d491769a08d3719257
Jacob Hamilton <jacobhami02@outlook.com>
2022-03-30 23:12:52 -0400

    margins

--- GameRoadMapWebsite/MainPage.php
+++ GameRoadMapWebsite/MainPage.php
@@ -3,12 +3,10 @@
 <link rel="stylesheet" href="styling.css">
 <title>Raindrop Roadmap</title>
 
-<body class='background'>
+<body class="background">
 <!-- <div id='rcorners'><h1 id='header'>Reign-drop Roadmap</h1></div> -->
 <ul>
-  <div>
-    <img src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg">
-  </div>
+  <li><a class="logo"></a></li>
   <li><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Contact Us</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About Us</a></li>
