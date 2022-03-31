fe3ba22aad9a56e137f0be3dbdfed37b6ae9d1da
Jacobhami02 <92939984+Jacobhami02@users.noreply.github.com>
2022-03-31 19:06:39 -0400

    quiz3

--- classwork/Quiz 3/carTaxCalulator.php
+++ classwork/Quiz 3/carTaxCalulator.php
@@ -1,14 +1,40 @@
 <?php
 
+function taxCalculator($yearPurchased, $yearSold, $salePrice, $costBasis, $ownerStatus, $incomeLevel){
 
-function taxCalculator($yearsOwned, $salePrice, $ownerStatus, $costBasis, ){
+$yearsOwned=$yearSold-$yearPurchased;
+
+if($ownerStatus === "single" && $incomeLevel > 50000){
+    $payTaxes=1;
+}
+elseif($ownerStatus === "single" && $incomeLevel <= 50000){
+    $payTaxes=0;
+}
+elseif($ownerStatus === "married" && $incomeLevel <= 80000){
+    $payTaxes=0;
+}
+else{
+    $payTaxes=1;
+}
 
 
+if ($payTaxes===1){
+    if($yearsOwned <=1){
+        $intTwo = 33 / $salePrice;
+        $finalTax = $intTwo * 100;
+    }
+    else{
+        $intOne = 15 / $salePrice;
+        $finalTax= $intOne * 100;
+    }
+}
+else{
+    $finalTax = 0;
+}
 
-var $finalTax;
     return $finalTax;
 }
 
-echo taxCalculator()
+echo taxCalculator(2020, 2022, 10000, 30000, "single", 70000);
 
 ?>
