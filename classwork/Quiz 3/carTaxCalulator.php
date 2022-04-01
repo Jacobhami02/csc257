8929affe3597cfe91912157fe1c250d1c26c5fab
Jacob Hamilton <jacobhami02@outlook.com>
2022-03-31 20:48:17 -0400

    quiz 3 fixes

--- classwork/Quiz 3/carTaxCalulator.php
+++ classwork/Quiz 3/carTaxCalulator.php
@@ -1,40 +1,27 @@
 <?php
-
 function taxCalculator($yearPurchased, $yearSold, $salePrice, $costBasis, $ownerStatus, $incomeLevel){
 
-$yearsOwned=$yearSold-$yearPurchased;
+$yearsOwned = $yearSold - $yearPurchased;
+$profit = $costBasis - $salePrice;
 
 if($ownerStatus === "single" && $incomeLevel > 50000){
-    $payTaxes=1;
-}
+    $payTaxes=1; }
 elseif($ownerStatus === "single" && $incomeLevel <= 50000){
-    $payTaxes=0;
-}
+    $payTaxes=0; }
 elseif($ownerStatus === "married" && $incomeLevel <= 80000){
-    $payTaxes=0;
-}
+    $payTaxes=0; }
 else{
-    $payTaxes=1;
-}
-
+    $payTaxes=1; }
 
 if ($payTaxes===1){
-    if($yearsOwned <=1){
-        $intTwo = 33 / $salePrice;
-        $finalTax = $intTwo * 100;
-    }
+    if($yearsOwned <= 1){
+        $finalTax = .33 * $salePrice; }
     else{
-        $intOne = 15 / $salePrice;
-        $finalTax= $intOne * 100;
-    }
-}
+        $finalTax= .15 * $salePrice; }}
 else{
-    $finalTax = 0;
-}
+    $finalTax = 0; }
 
-    return $finalTax;
-}
+    return $finalTax; }
 
-echo taxCalculator(2020, 2022, 10000, 30000, "single", 70000);
-
+echo taxCalculator(2020, 2022, 28000, 30000, "married", 20000);
 ?>
