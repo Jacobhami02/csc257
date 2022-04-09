e5930b6d0f0e062d92fb42c11a4e93b15f6df7da
Jacob Hamilton <jacobhami02@outlook.com>
2022-04-09 00:49:40 -0400

    NavBar touch up

--- GameRoadMapWebsite/MainPage.php
+++ GameRoadMapWebsite/MainPage.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-<link rel="stylesheet" href="styling.css">
+<link rel="stylesheet" href="styles.css">
 <title>Raindrop Roadmap</title>
 
-<body class="background">
 <!-- <div id='rcorners'><h1 id='header'>Reign-drop Roadmap</h1></div> -->
+<div id="topNav">
 <ul>
   <div class="logo">
-    <img  src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg">
+    <!-- <img  src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg"> -->
   </div>
+  
   <li><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Contact Us</a></li>
   <li><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About Us</a></li>
 </ul>
+</div>
+<body class="background">
 <div>
   <!-- <img src="images/rose.png"> -->
 </div>
