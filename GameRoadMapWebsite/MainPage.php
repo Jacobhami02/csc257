694e4cba19dd102d6f9530e14de151227766196a
Jacob Hamilton <jacobhami02@outlook.com>
2022-04-10 17:12:29 -0400

    topNav completion

--- GameRoadMapWebsite/MainPage.php
+++ GameRoadMapWebsite/MainPage.php
@@ -2,14 +2,15 @@
 <html lang="en">
 <link rel="stylesheet" href="styles.css">
 <title>Raindrop Roadmap</title>
-<!-- TopNav -->
-<ul>
-  <li><img class="logo" src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg"></li>
-  <li id="top"><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
-  <li id="top"><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Suggestions</a></li>
-  <li id="top"><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About The Game</a></li>
-</ul>
 <body class="background">
+
+<!-- TopNav -->
+<ul id="topNav">
+  <li><img class="logo" src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg"></li>
+  <li id="topButtonSpacing"><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
+  <li id="topButtonSpacing"><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Suggestions</a></li>
+  <li id="topButtonSpacing"><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About The Game</a></li>
+</ul>
 <div>
   <!-- <img src="images/rose.png"> -->
 </div>
