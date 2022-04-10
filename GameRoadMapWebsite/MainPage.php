3797f2a9e6979ec186571987849ee155801061af
Jacob Hamilton <jacobhami02@outlook.com>
2022-04-10 16:58:41 -0400

    visual fixes

--- GameRoadMapWebsite/MainPage.php
+++ GameRoadMapWebsite/MainPage.php
@@ -5,11 +5,10 @@
 <!-- TopNav -->
 <ul>
   <li><img class="logo" src="/csc257/GameRoadMapWebsite/images/RainDropLogo.jpg"></li>
-  <li><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
-  <li><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Contact Us</a></li>
-  <li><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About Us</a></li>
+  <li id="top"><a href="/csc257/GameRoadMapWebsite/MainPage.php">Home</a></li>
+  <li id="top"><a href="/csc257/GameRoadMapWebsite/suggestionForm.php">Suggestions</a></li>
+  <li id="top"><a href="/csc257/GameRoadMapWebsite/aboutDevPage.php">About The Game</a></li>
 </ul>
-<!-- <div id='rcorners'><h1 id='header'>Reign-drop Roadmap</h1></div> -->
 <body class="background">
 <div>
   <!-- <img src="images/rose.png"> -->
